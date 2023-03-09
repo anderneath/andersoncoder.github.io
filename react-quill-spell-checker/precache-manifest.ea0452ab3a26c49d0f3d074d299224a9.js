b7dde341db15ea2971c41c0ee1b5b2ca1ac965ed
Anderson Filho <andersonsilva@estantemagica.com.br>
2023-03-08 21:17:43 -0300

    feat: deploy to GitHub Pages

---
+++ react-quill-spell-checker/precache-manifest.ea0452ab3a26c49d0f3d074d299224a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e0175228dcb9d05d1ae7394d78e5ed",
+    "url": "/react-quill-spell-checker/index.html"
+  },
+  {
+    "revision": "a45263e4180127146b46",
+    "url": "/react-quill-spell-checker/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "5e88fdac02242bd508cb",
+    "url": "/react-quill-spell-checker/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "a45263e4180127146b46",
+    "url": "/react-quill-spell-checker/static/js/2.a8c8af95.chunk.js"
+  },
+  {
+    "revision": "39437e89a4c49cb6cd4ecef99b560105",
+    "url": "/react-quill-spell-checker/static/js/2.a8c8af95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e88fdac02242bd508cb",
+    "url": "/react-quill-spell-checker/static/js/main.a34a737d.chunk.js"
+  },
+  {
+    "revision": "e7e42fbf406ce2e0bd36",
+    "url": "/react-quill-spell-checker/static/js/runtime-main.2bee6cdf.js"
+  }
+]);
